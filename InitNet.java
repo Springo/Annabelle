066f79ed5615edb9f671e85c727c347279da4c84
Springo <kevinmxia@gmail.com>
2016-09-05 01:43:58 -0400

    Finished neural net constructor

--- InitNet.java
+++ InitNet.java
@@ -1,9 +1,6 @@
 public class InitNet {
 	public static void main(String [] args) {
-		double [] test = {1.0, 0.5, 0.5};
-		double [] test2 = {5, -2};
-		Perceptron tester = new Perceptron(test);
-		tester.updateValue(0.0, test2);
-		System.out.println(tester.getValue(0));
+		int [] test = {3, 10, 1};
+		NeuralNet annabelle = new NeuralNet(3, test);
 	}
 }
--- NeuralNet.java
+++ NeuralNet.java
@@ -1,7 +1,27 @@
-public class NeuralNet {
-	
+public class NeuralNet {	
 	private int layers;
+	private int [] layersizes;
+	private Perceptron [][] network;
 
-	public NeuralNet () {
+	public NeuralNet (int [] numunits) {
+		this(numunits.length, numunits);
+	}
+
+	public NeuralNet (int numlay, int [] numunits) {
+		layers = numlay;
+		layersizes = numunits;
+		network = new Perceptron[numlay][];
+		for (int i = 0; i < layers; i++) {
+			network[i] = new Perceptron[layersizes[i]];
+		}
+		for (int i = 0; i < layers - 1; i++) {
+			for (int j = 0; j < layersizes[i]; j++) {
+				network[i][j] = new Perceptron(layersizes[i + 1]);
+			}
+		}
+		for (int j = 0; j < layersizes[layers - 1]; j++) {
+			double[] unitweight = {1};
+			network[layers - 1][j] = new Perceptron(unitweight);
+		}
 	}
 }
